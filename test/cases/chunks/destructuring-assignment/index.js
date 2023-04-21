e3dd0742a91dc7cd3f25b8ea36ac0aa0ba441f94
Jesse van Assen <jesse.v.assen@gmail.com>
2023-04-21 19:30:59 +0200

    Add tests for destructuring assignment of dynamic json imports

--- test/cases/chunks/destructuring-assignment/index.js
+++ test/cases/chunks/destructuring-assignment/index.js
@@ -10,3 +10,18 @@ it("should get warning on using 'webpackExports' with destructuring assignment",
 	expect(def).toBe(3);
 	done();
 });
+
+it("should support importing objects from JSON files", async () => {
+	const { default: json } = await import("./dir1/object.json");
+	expect(json).toBe({ a: 1 });
+});
+
+it("should support importing arrays from JSON files", async () => {
+	const { default: json } = await import("./dir1/array.json");
+	expect(json).toBe(["a"]);
+});
+
+it("should support importing primitives from JSON files", async () => {
+	const { default: json } = await import("./dir1/primitive.json");
+	expect(json).toBe("a");
+});
